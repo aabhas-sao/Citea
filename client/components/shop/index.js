1be5da845e42a3a5034ee4effe4bf1113dbd876e
Aabhas Sao <aabhassao0@gmail.com>
2023-03-20 02:37:08 +0530

    feat: Add navigation tabs
    Signed-off-by: Aabhas Sao <aabhassao0@gmail.com>

---
+++ client/components/shop/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+const ShopScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Shop Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default ShopScreen;
